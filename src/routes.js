30159f6f3bcc22f904c00183b8c0da7669abf2f3
LeuAlmeida <leo@webid.net.br>
2019-10-21 16:05:15 -0300

    First route using Sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World ' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leonardo Almeida',
+    email: 'leo@webid.net.br',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
