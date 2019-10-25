b743444ab7e702fefbb42621a64a9ca4c9fec4a6
LeuAlmeida <leo@webid.net.br>
2019-10-25 12:52:58 -0300

    My notes

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,14 @@
 export default (req, res, next) => {
+  // Recebe o token através do header Authorization
   const authHeader = req.headers.authorization;
 
-  console.log(authHeader);
+  // Verifica a existência do token
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided.' })
+  }
+
+  // Recebe o token e dá um split para remover o "Bearer" e manter apenas o token.
+  const [, token] = authHeader.split(' ');
 
   return next();
 };
