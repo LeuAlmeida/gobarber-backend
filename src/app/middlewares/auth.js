64170efa090e18ad68effdb34337ae3de9750897
LeuAlmeida <leo@webid.net.br>
2019-10-25 13:54:00 -0300

    Increment auth middleware

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,4 +1,9 @@
-export default (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
   // Recebe o token através do header Authorization
   const authHeader = req.headers.authorization;
 
@@ -10,5 +15,19 @@ export default (req, res, next) => {
   // Recebe o token e dá um split para remover o "Bearer" e manter apenas o token.
   const [, token] = authHeader.split(' ');
 
-  return next();
+  // Tentativa de descriptografar a senha
+  try {
+    // Decodificação da senha, promisificando a verificação do JsonWebTOken. Essa promise retorna uma função, que retornará o token e receberá a configuração do authConfigs.secret
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    // Inclui o id do usuário dentro do req
+    req.userId = decoded.id;
+
+    // Se chegou no next(), o usuário está pronto para acessar ao controller
+    return next();
+
+  } catch (err) {
+    // Em caso de erro, deve retornar a mensagem abaixo
+    return res.status(401).json({ error: 'Token invalid '})
+  }
 };
