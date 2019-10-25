c33c0629d4b84db5f210c5d1380e739107e6bc15
LeuAlmeida <leo@webid.net.br>
2019-10-25 14:32:48 -0300

    Finish the user update controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,9 +19,37 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId)
+    // Abstrair o e-mail e a senha antiga do corpo da requisição
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    // Buscar o usuário que quer ser ediado dentro do banco de dados
+    const user = await User.findByPk(req.userId);
+
+
+    // Verificação para alteração do e-mail
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    // Verificação para alteração de senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match. '});
+    }
+
+    // Atualiza as informações conforme o usuário enviar pelo corpo da requisição.
+    const { id, name, provider } = await user.update(req.body);
+
+    // Retorna os dados do usuário
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
