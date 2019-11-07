95807f4157ae1787f15823e8e95c7c98ce90c3ad
LeuAlmeida <leo@webid.net.br>
2019-11-07 13:21:43 -0300

    Create upload files route

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-// Controller de Usuário
 import UserController from './app/controllers/UserController';
-// Controller de Sessão
 import SessionController from './app/controllers/SessionController';
 
-// Middleware de Autenticação
 import authMiddleware from './app/middlewares/auth';
 
-// Criação do método de rotas
 const routes = new Router();
+const upload = multer(multerConfig);
 
-// Rota para criação de usuário
 routes.post('/users', UserController.store);
-
-// Rota de login
 routes.post('/sessions', SessionController.store);
 
-// Utilização global do middleware de autenticação
 routes.use(authMiddleware);
 
-// Rota para editar usuário
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true })
+})
 
 export default routes;
