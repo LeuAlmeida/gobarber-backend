52c081cc8f037f682d57900c42523b5fc95d09b5
Léu Almeida <leo@webid.net.br>
2019-11-09 23:21:05 -0300

    Update file model

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,33 +1,19 @@
 import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcryptjs';
 
-class User extends Model {
+class File extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
-        password_hash: Sequelize.STRING,
-        provider: Sequelize.BOOLEAN,
+        path: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
 
-    this.addHook('beforeSave', async user => {
-      if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 8);
-      }
-    });
-
     return this;
-  }
-
-  checkPassword(password) {
-    return bcrypt.compare(password, this.password_hash);
   }
 }
 
-export default User;
+export default File;
