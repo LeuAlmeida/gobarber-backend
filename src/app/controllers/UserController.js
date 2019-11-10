c2d83467eba876f590eb8ec29b8d80208d4538e1
Léu Almeida <leo@webid.net.br>
2019-11-09 23:27:18 -0300

    Remove my notes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,7 +3,6 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    // Esquema de validação de usuário utilizando funções do Yup
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string()
@@ -35,7 +34,6 @@ class UserController {
   }
 
   async update(req, res) {
-    // Esquema de validação de usuário utilizando funções do Yup
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
@@ -54,13 +52,10 @@ class UserController {
       return res.status(400).json({ error: 'Validation fails.' });
     }
 
-    // Abstrair o e-mail e a senha antiga do corpo da requisição
     const { email, oldPassword } = req.body;
 
-    // Buscar o usuário que quer ser ediado dentro do banco de dados
     const user = await User.findByPk(req.userId);
 
-    // Verificação para alteração do e-mail
     if (email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
@@ -69,15 +64,12 @@ class UserController {
       }
     }
 
-    // Verificação para alteração de senha
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match. ' });
     }
 
-    // Atualiza as informações conforme o usuário enviar pelo corpo da requisição.
     const { id, name, provider } = await user.update(req.body);
 
-    // Retorna os dados do usuário
     return res.json({
       id,
       name,
