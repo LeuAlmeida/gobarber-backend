b5a09d1ff38d58ef7641ab21f610369e31064d61
Léu Almeida <leo@webid.net.br>
2019-11-09 23:27:07 -0300

    Update add avatar field to users migration

--- src/database/migrations/20191110022130-add-avatar-field-to-users.js
+++ src/database/migrations/20191110022130-add-avatar-field-to-users.js
@@ -1,23 +1,15 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
 };
