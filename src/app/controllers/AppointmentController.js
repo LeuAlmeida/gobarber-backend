3c4e993eaeca1631c067b53549263b2d7c14eec2
LeuAlmeida <leo@webid.net.br>
2019-11-18 17:59:26 -0300

    Create check availability method

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -17,8 +17,8 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
-    /*
-     *  Check if provider_id is a provider
+    /**
+     * Check if provider_id is a provider
      */
 
     const isProvider = await User.findOne({
@@ -31,10 +31,31 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers.' });
     }
 
+    /**
+     * Check for past dates
+     */
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'Past dates are not permitted.' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available.' });
     }
 
     const appointment = await Appointment.create({
