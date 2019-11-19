64c7fbea4dfd9bdf5e87296bdecce44efc979b6b
LeuAlmeida <leo@webid.net.br>
2019-11-19 15:16:51 -0300

    Create schedule route

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-import ScheaduleController from './app/controllers/ScheaduleController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -26,7 +26,7 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
-routes.get('/schedule', ScheaduleController.index);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
