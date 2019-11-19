3dbae2f5455e2a06e97db9869805dd8db4c85f13
LeuAlmeida <leo@webid.net.br>
2019-11-19 15:44:37 -0300

    Update index database with mongoDB struture

--- src/database/index.js
+++ src/database/index.js
@@ -26,7 +26,11 @@ class Database {
   mongo() {
     this.mongoConnection = mongoose.connect(
       'mongodb://localhost:27017/gobarber',
-      { useNewUrlParser: true, useFindAndModify: true }
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
     );
   }
 }
