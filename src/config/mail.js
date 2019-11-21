3321489ba029a6d78d8f553f39881c3373dac2f8
LeuAlmeida <leo@webid.net.br>
2019-11-21 17:34:10 -0300

    Incrementing mail settings with mailtrap infos

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: '',
-  port: '',
+  host: 'smtp.mailtrap.io',
+  port: '2525',
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: 'e7319c852647bd',
+    pass: '128efaea5c99e6',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
