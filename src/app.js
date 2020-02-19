73eee1faaf2c09559d8d1fb8ac68e65a14d8a1c2
LeuAlmeida <leo@webid.net.br>
2020-02-19 12:55:07 -0300

    feat(app): applying cors as middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
+
 import routes from './routes';
 import sentryConfig from './config/sentry';
 
@@ -23,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
